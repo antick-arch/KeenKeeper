abbeb7a1a98bd9779938f6c826625df05bbe2409
antick-arch <e241003@grad.iiuc.ac.bd>
2026-05-11 20:18:53 +0600

    Add react-icons dependency, update layout and navbar components, and include new asset images

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavbarPage from "@/components/shared/Navbar";
 import FooterPage from "@/components/shared/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Geist } from 'next/font/google'
+ 
+const geist = Geist({
+  subsets: ['latin'],
+})
 
 export const metadata = {
   title: "KeenKeeper | Homepage",
@@ -23,9 +17,9 @@ export default function RootLayout({ children }) {
     <html
       lang="en"
       data-theme="light"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={geist.className}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full">
         <NavbarPage></NavbarPage>
         {children}
         <FooterPage></FooterPage>
