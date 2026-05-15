945802d36257ccdd01f46527adba0a8039ec78d0
antick-arch <e241003@grad.iiuc.ac.bd>
2026-05-15 11:16:27 +0600

    Enhance StatsPage header styling for improved visibility

--- src/app/stats/page.jsx
+++ src/app/stats/page.jsx
@@ -6,7 +6,7 @@ const StatsPage = () => {
         <div className="max-w-277.5 mx-auto my-5 md:my-20">
             <h2 className="text-2xl md:text-4xl font-bold mb-6">Friendship Analytics</h2>
             <div className="bg-white shadow rounded-lg p-6">
-                <h2 className='font-medium text-xl'>By Interaction Type</h2>
+                <h2 className='font-medium text-xl text-[#244D3F]'>By Interaction Type</h2>
                 <PieChartWithPaddingAngle />
             </div>
         </div>
